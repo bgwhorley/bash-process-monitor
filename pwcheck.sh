a3dffbb94f66c759ed617c8afe423f92c19ddc51
bwhorley <bwhorley@purdue.edu>
2016-02-05 14:18:30 -0500

    Lab 2 commit

---
+++ pwcheck.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SCORE=0
+if [ ${#1} -lt 6 AND ${#1} -gt 32 ]; then
+  echo "Error: Password length invalid."
+else 
+  $SCORE=32-$#1
+fi
+
+
+#DO NOT REMOVE THE FOLLOWING TWO LINES
+git add $0 >> .local.git.out
+git commit -a -m "Lab 2 commit" >> .local.git.out
+
+#Your code here
