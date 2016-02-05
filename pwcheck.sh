ac2a52330ff44589b1ea02c544d04727cb763cea
bwhorley <bwhorley@purdue.edu>
2016-02-05 14:21:43 -0500

    Lab 2 commit

--- pwcheck.sh
+++ pwcheck.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 SCORE=0
+if [ $# -lt 1 ]; then
+  echo "Error: Please enter a password"
+fi
 if [ ${#1} -lt 6 AND ${#1} -gt 32 ]; then
   echo "Error: Password length invalid."
 else 
-  $SCORE=32-$#1
+  $SCORE=32-${#1}
 fi
 
 
