7b3b27f425469554a9a18c0995b6096457f9a317
bwhorley <bwhorley@purdue.edu>
2016-02-05 14:35:14 -0500

    Lab 2 commit

--- pwcheck.sh
+++ pwcheck.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-SCORE=0
-if [ $# -lt 1 ]; then
-  echo "Error: Please enter a password"
-fi
-if [ ${#1} -lt 6 AND ${#1} -gt 32 ]; then
-  echo "Error: Password length invalid."
-else 
-  $SCORE=32-${#1}
-fi
-
-
 #DO NOT REMOVE THE FOLLOWING TWO LINES
 git add $0 >> .local.git.out
 git commit -a -m "Lab 2 commit" >> .local.git.out
 
 #Your code here
+SCORE=0
+if [ $# -lt 1 ]; then
+  echo "Error: Please enter a password"
+fi
+if [ ${#1} -lt 6 ]; then
+  echo "Error: Password length invalid"
+else if [ ${#1} -gt 32 ]; then
+  echo "Error: Password length invalid."
+else 
+  $SCORE=32-${#1}
+fi
+
+echo $SCORE
+
